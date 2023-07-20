9e7f42b7eefbfd56e1315d1c28f3657366db3f30
aki <akityablog@gmail.com>
2023-07-20 23:47:51 +0900

    更新

--- main.go
+++ main.go
@@ -213,4 +213,14 @@ func main() {
 	for j := range arr {
 		fmt.Println(j)
 	}
+
+	// switch式
+	switch sw := 1; sw {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("それ以外")
+	}
 }
